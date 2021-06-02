8c64e79e0b04c18c59714c8ec7e9126146ea603c
Daniel Recuenco <dani@kedarastudios.com>
2021-06-02 16:39:07 +0200

    rename crowdin.yml and write translations

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,27 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
+import Translate from '@docusaurus/Translate';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title">        
+          <Translate description="The homepage main hero heading" id="homepage.hero.title">
+              Armada Alliance Docs
+          </Translate></h1>
+        <p className="hero__subtitle">
+          <Translate description="The homepage main hero tagline" id="homepage.hero.tagline">
+          ARM powered Cardano Stake Pool Guides
+          </Translate>
+        </p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/README">
-            Start Learning
+            <Translate description="The homepage main hero button text" id="homepage.hero.button_text">Start Learning</Translate>
           </Link>
         </div>
       </div>
@@ -29,8 +37,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="ARM powered Cardano Stake Pool Guides">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
